9ac9c5d7b931fd61a26a56ee69cd08e930a0b508
Jeff Martin <jeff@reportmill.com>
2024-01-20 14:24:08 -0600

    CJProcess: Slight fix for running at rm

--- src/snapcj/CJProcess.java
+++ src/snapcj/CJProcess.java
@@ -49,8 +49,12 @@ public class CJProcess extends Process {
 
         // If UseCJDom, add CJDom and SnapCJ
         _useCJDom = args[0].equals("java-dom");
-        if (_useCJDom)
-            _classPath = "/app/CJDom-2024.01.jar:/app/SnapCJ-2024.01.jar:" + _classPath;
+        if (_useCJDom) {
+            String cjdomJars = "/app/CJDom-2024.01.jar:/app/SnapCJ-2024.01.jar:";
+            if (_classPath.contains("app/SnapCode/app"))
+                cjdomJars = cjdomJars.replace("/app/", "/app/SnapCode/app/");
+            _classPath = cjdomJars + _classPath;
+        }
 
         System.out.println("MainClass: " + _mainClassName);
         System.out.println("ClassPath: " + _classPath);
