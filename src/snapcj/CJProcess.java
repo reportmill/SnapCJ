8389874d4178f8fada6d73472a9477a8531133e8
Jeff Martin <jeff@reportmill.com>
2024-05-01 15:08:22 -0500

    Slight fix

--- src/snapcj/CJProcess.java
+++ src/snapcj/CJProcess.java
@@ -51,7 +51,7 @@ public class CJProcess extends Process {
         // If UseCJDom, add CJDom and SnapCJ
         _useCJDom = args[0].equals("java-dom");
         if (_useCJDom) {
-            String cjdomJars = "/app/CJDom-2024.04.jar:/app/SnapCJ-2024.04.jar:";
+            String cjdomJars = "/app/CJDom-2024.05.jar:/app/SnapCJ-2024.05.jar:";
             if (_classPath.contains("app/SnapCode/app"))
                 cjdomJars = cjdomJars.replace("/app/", "/app/SnapCode/app/");
             _classPath = cjdomJars + _classPath;
