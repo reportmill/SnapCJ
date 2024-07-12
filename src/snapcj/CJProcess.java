452139b586c300e42d22e9b99e7971c48b9309c2
Jeff Martin <jeff@reportmill.com>
2024-07-12 17:00:58 -0500

    Update jar refs

--- src/snapcj/CJProcess.java
+++ src/snapcj/CJProcess.java
@@ -58,7 +58,7 @@ public class CJProcess extends Process {
         // If UseCJDom, add CJDom and SnapCJ
         _useCJDom = args[0].equals("java-dom");
         if (_useCJDom) {
-            String cjdomJars = "/app/CJDom-2024.06.jar:/app/SnapCJ-2024.06.jar:";
+            String cjdomJars = "/app/CJDom-2024.07.jar:/app/SnapCJ-2024.07.jar:";
             if (_classPath.contains("app/SnapCode/app"))
                 cjdomJars = cjdomJars.replace("/app/", "/app/SnapCode/app/");
             _classPath = cjdomJars + _classPath;
